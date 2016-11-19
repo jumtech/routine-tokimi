fc5bcd63b6d5ab2c8053ffe054f11937ac3eba1f
hkdnet <hkdnet@users.noreply.github.com>
2016-11-19 19:48:59 +0900

    add messages

---
+++ app/models/MessageEvent.js
@@ -0,0 +1,16 @@
+'use strict';
+const WebhookEvent = require('./WebhookEvent.js');
+const MessageFactory = require('./MessageFactory.js');
+
+class MessageEvent extends WebhookEvent {
+  get replyToken() { return this.data.replyToken; }
+  get message() {
+    if (this._message) {
+      return this._message;
+    }
+    this._message = MessageFactory.create(this.data['message']);
+    return this._message;
+  }
+}
+
+module.exports = MessageEvent;
