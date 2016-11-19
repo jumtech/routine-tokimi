e1fc9b2e1909b322f443dab263234f6e2684090e
jumtech <despairing.clock@gmail.com>
2016-11-19 17:28:28 +0900

    fix reply function

--- index.js
+++ index.js
@@ -5,24 +5,12 @@ const request = require('request');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-app.use(morgan({ format: 'dev', immediate: true}));
+app.use(morgan("dev", {immediate: true}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-const headers = {
-  "Content-Type" : "application/json; charset=UTF-8",
-  "Authorization" : "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
-};
-
-const options = {
-  "url" : "https://api.line.me/v2/bot/message/reply",
-  "method" : "post",
-  "headers": headers,
-  "json": true,
-  "payload": null
-}
 
 app.post('/callback', function(req, res, next) {
   console.log("receive POST...");
@@ -30,6 +18,7 @@ app.post('/callback', function(req, res, next) {
   events.forEach(function(event) {
     console.log("forEach");
     if (event.type == "message") {
+      var url = "https://api.line.me/v2/bot/message/reply";
       var postData = {
         "replyToken" : event.replyToken,
         "messages" : [
@@ -39,9 +28,14 @@ app.post('/callback', function(req, res, next) {
           }
         ]
       };
-      console.log(event);
-      options.payload = JSON.stringify(postData);
-      request.post(options, function (err, res, body) {
+      var options = {
+        "headers": {
+          "Content-Type" : "application/json; charset=UTF-8",
+          "Authorization" : "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
+        },
+        "payload": JSON.stringify(postData)
+      }
+      request.post(url, options, function (err, res, body) {
         if (!err && res.statusCode == 200) {
           console.log(body);
         } else {
@@ -54,6 +48,7 @@ app.post('/callback', function(req, res, next) {
       /* ブロック */
     }
   });
+  // Web hookへのリクエストに200を返す
   res.statusCode = 200;
   res.end();
 });
