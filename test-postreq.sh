299ae3f44f70d7a3440d73d685198fa4b7c384f1
jumtech <despairing.clock@gmail.com>
2016-11-19 22:02:53 +0900

    enable to use attribute to change message from user in reply test

--- test-postreq.sh
+++ test-postreq.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-curl -X POST http://localhost:8080/callback -H "Accept: application/json" -H "Content-type: application/json" -d '{"events": [{"replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA","type": "message","timestamp": 1462629479859,"source": { "type": "user", "userId": "U206d25c2ea6bd87c17655609a1c37cb8" }, "message": { "id": "325708", "type": "text", "text": "Hello, world"}}]}'
+
+if [ $# -eq 0 ]; then
+  TEXT="Hello world!"
+  echo "ユーザーからのメッセージ文言を第一引数で渡せますよ。"
+  echo "defaultは、\"${TEXT}\"です"
+else
+  TEXT=$1
+fi
+echo "送信メッセージ：$TEXT"
+JSON="{\"events\": [{\"replyToken\": \"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA\",\"type\": \"message\",\"timestamp\": 1462629479859,\"source\": { \"type\": \"user\", \"userId\": \"U206d25c2ea6bd87c17655609a1c37cb8\" }, \"message\": { \"id\": \"325708\", \"type\": \"text\", \"text\": \"$TEXT\"}}]}"
+curl -X POST http://localhost:8080/callback -H "Accept: application/json" -H "Content-type: application/json" -d "$JSON"
