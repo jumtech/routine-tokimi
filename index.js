dfaa2b3ea4587bdd6f9f5ab4fa5b88adc1007d47
jumtech <despairing.clock@gmail.com>
2016-11-19 22:06:17 +0900

    add feature to change replyText accoring to gotText

--- index.js
+++ index.js
@@ -11,46 +11,78 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-
 app.post('/callback', function(req, res, next) {
   var events = req.body.events;
   events.forEach(function(event) {
-    if (event.type == "message") {
-      var postData = {
-        "replyToken" : event.replyToken,
-        "messages" : [
-          {
-            "type" : "text",
-            "text" : ((event.message.type=="text") ? event.message.text : "ちょっと難しいなあ")
-          }
-        ]
-      };
-      var options = {
-        url: "https://api.line.me/v2/bot/message/reply",
-        headers: {
-          "Content-Type" : "application/json; charset=UTF-8",
-          "Authorization" : "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
-        },
-        json: true,
-        body: postData
-      };
-      request.post(options, function (err, res, body) {
-        if (!err && res.statusCode == 200) {
-          console.log(body);
+    switch (event.type) {
+      case "message":
+      // ユーザーから送られたテキスト
+      var gotText = "";
+      // ユーザーに返す
+      var replyText = "";
+        if (event.message.type = "text") {
+        // テキストが送られた場合
+          gotText = event.message.text;
+          replyText = _getSendText(gotText);
+          console.log("replyText:" + replyText);
         } else {
-          console.log('error: ' + JSON.stringify(res));
+        // スタンプ等が送られた場合
+          replyText = "ちょっと難しいなあ";
         }
-      });
-    } else if (event.type == "follow") {
-      /* 友達追加・ブロック解除 */
-    } else if (event.type == "unfollow") {
-      /* ブロック */
+        var gotText = event.message.text;
+        var postData = {
+          "replyToken" : event.replyToken,
+          "messages" : [
+            {
+              "type" : "text",
+              "text" : replyText
+            }
+          ]
+        };
+        var options = {
+          url: "https://api.line.me/v2/bot/message/reply",
+          headers: {
+            "Content-Type" : "application/json; charset=UTF-8",
+            "Authorization" : "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
+          },
+          json: true,
+          body: postData
+        };
+        request.post(options, function (err, res, body) {
+          if (!err && res.statusCode == 200) {
+            console.log(body);
+          } else {
+            console.log('error: ' + JSON.stringify(res));
+          }
+        });
+        break;
+      case "follow":
+        /* 友達追加・ブロック解除 */
+        break;
+      case "unfollow":
+        /* ブロック */
+        break;
     }
   });
   // Web hookへのリクエストに200を返す
   res.statusCode = 200;
   res.end();
+  next();
 });
+
+_getSendText = function(gotText) {
+  var replyText = "ん？バグかな？";
+  if (gotText.includes("登録")) {
+    replyText = "新しく登録するルーチン名を入力してね。短い方が覚えやすいから嬉しいな";
+  } else if (gotText.includes("やる")) {
+    replyText = "ルーチンを始めるんだね！頑張ろう！";
+  } else if (gotText.includes("みる")) {
+    replyText = "ルーチンに登録されているタスクは、こんな感じだよー";
+  } else {
+    replyText = "よくわからんちー";
+  }
+  return replyText;
+};
 
 app.get('/test', function(req, res) {
   res.send("TOKIMI is AWAKE");
