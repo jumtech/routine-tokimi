e7826d95e7baa563381a27c1e9c2029aa15695b2
hkdnet <hkdnet@users.noreply.github.com>
2016-11-19 18:56:10 +0900

    Add WebhookEvent

--- test/models/WebhookEventTest.js
+++ test/models/WebhookEventTest.js
@@ -1,7 +1,26 @@
 const assert = require('power-assert');
+const WebhookEvent = require('../../app/models/WebhookEvent.js');
 
-describe('test', function() {
-  it('fail', ()=> {
-    assert(1 == 2);
+describe('WebhookEvent', function() {
+  describe('constructor', function() {
+    before(function() {
+      this.data = {
+        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+        "type": "message",
+        "timestamp": 1462629479859,
+        "source": {
+          "type": "user",
+          "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+        },
+        "message": {
+          "id": "325708",
+          "type": "text",
+          "text": "Hello, world"
+        }
+      };
+      this.subject = new WebhookEvent(this.data);
+    });
+    it('type', function() { assert(this.subject.type == "message") });
+    it('timestamp', function() { assert(this.subject.timestamp == 1462629479859) });
   });
 });
