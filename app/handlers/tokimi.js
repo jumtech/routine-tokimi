534657c4686ebff79520db83250bbb514d698088
jumtech <despairing.clock@gmail.com>
2016-11-20 10:41:13 +0900

    add ADD mode to tokimi

--- app/handlers/tokimi.js
+++ app/handlers/tokimi.js
@@ -4,6 +4,7 @@ const ReplySender = require('../models/ReplySender.js');
 const ReplySenderConfig = require('../models/ReplySenderConfig.js');
 const config = new ReplySenderConfig({token: process.env.CHANNEL_ACCESS_TOKEN});
 const sender = new ReplySender(config);
+var mode = "NORMAL";
 
 module.exports = function (req, res, next) {
   // Web hookへのリクエストに200を返す
@@ -52,63 +53,108 @@ module.exports = function (req, res, next) {
 };
 
 function _makeSendMessages (gotText) {
-  const ADD = /登録|^と$/;
-  const RUN = /やる/;
-  const LIST = /みる|見る/;
   var replyMessages = [];
 
-  // ルーチン登録
-  if (gotText.search(ADD) !== -1) {
-    replyMessages = [
-      {
-        "type": "text",
-        "text": "新しく登録するルーチン名を入力してね。"
-      },
-      {
-        "type": "text",
-        "text": "短い方が覚えやすいから嬉しいな"
+  // 現在のモードで分岐
+  switch (mode) {
+    case "NORMAL":
+      // 通常モード：メッセージ内容に応じてモード変更
+      const ADD = /登録|^と$/;
+      const RUN = /やる/;
+      const LIST = /みる|見る/;
+      if (gotText.search(ADD) !== -1) {
+        mode = "ADD";
+        replyMessages = [
+          {
+            "type": "text",
+            "text": "新しく登録するルーチン名を入力してね。"
+          },
+          {
+            "type": "text",
+            "text": "短い方が覚えやすいから嬉しいな"
+          }
+        ];
+        return replyMessages;
       }
-    ];
-    return replyMessages;
-  }
+      var RUNindex = gotText.search(RUN);
+      if (RUNindex !== -1) {
+        mode = "RUN";
+        replyMessages = [
+          {
+            "type": "text",
+            "text": gotText.substring(0, RUNindex) + "のルーチンを始めるんだね！"
+          },
+          {
+            "type": "text",
+            "text": "頑張ろう！"
+          }
+        ];
+        return replyMessages;
+      }
 
-  // ルーチン実行
-  var RUNindex = gotText.search(RUN);
-  if (RUNindex !== -1) {
-    replyMessages = [
-      {
-        "type": "text",
-        "text": gotText.substring(0, RUNindex) + "のルーチンを始めるんだね！"
-      },
-      {
-        "type": "text",
-        "text": "頑張ろう！"
+      var LISTindex = gotText.search(LIST);
+      if (LISTindex !== -1) {
+        mode = "LIST";
+        replyMessages = [
+          {
+            "type": "text",
+            "text": gotText.substring(0, LISTindex) + "のルーチンに登録されているタスクは、こんな感じだよー"
+          },
+          {
+            "type": "text",
+            "text": "頑張ろう！"
+          }
+        ];
+        return replyMessages;
       }
-    ];
-    return replyMessages;
+      // default
+      mode = "NORMAL";
+      replyMessages = [
+        {
+          "type": "text",
+          "text": "新しくルーチンを登録するには、「登録」とか「と」とか言ってね。"
+        }
+      ];
+      return replyMessages;
+    case "ADD":
+      replyMessages = _makeADDMessages(gotText);
+      return replyMessages;
+    default:
+      mode = "NORMAL";
+      replyMessages = [
+        {
+          "type": "text",
+          "text": "うーん、バグかな？困ったなあ。"
+        }
+      ];
+      return replyMessages;
   }
+};
 
-  // ルーチン確認
-  var LISTindex = gotText.search(LIST);
-  if (LISTindex !== -1) {
+function _makeADDMessages (gotText) {
+  var replyMessages = [];
+  if (gotText.includes("終了")) {
     replyMessages = [
       {
         "type": "text",
-        "text": gotText.substring(0, LISTindex) + "のルーチンに登録されているタスクは、こんな感じだよー"
+        "text": "新しいルーチンはこんな感じだよ！"
       },
       {
         "type": "text",
-        "text": "頑張ろう！"
+        "text": "ルーチン名：朝\n顔を洗う\n歯を磨く\n二度寝する"
+      },
+      {
+        "type": "text",
+        "text": "一緒に頑張ろうね！"
       }
     ];
     return replyMessages;
   }
-  // default
   replyMessages = [
     {
       "type": "text",
-      "text": "新しくルーチンを登録するには、「登録」とか「と」とか言ってね。"
+      "text": "おっけー。まだタスクあったら、教えてー"
     }
   ];
   return replyMessages;
-};
+}
