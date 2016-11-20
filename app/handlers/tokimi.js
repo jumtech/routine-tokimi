910c5a75da345afe4bbacd7910b23383a0b8869d
jumtech <despairing.clock@gmail.com>
2016-11-20 09:19:31 +0900

    add feature to send multiple messages

---
+++ app/handlers/tokimi.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const request = require('request');
+const ReplySender = require('../models/ReplySender.js');
+const ReplySenderConfig = require('../models/ReplySenderConfig.js');
+const config = new ReplySenderConfig({token: process.env.CHANNEL_ACCESS_TOKEN});
+const sender = new ReplySender(config);
+
+module.exports = function (req, res, next) {
+  // Web hookへのリクエストに200を返す
+  res.statusCode = 200;
+  res.end();
+
+  var events = req.body.events;
+  events.forEach(function(event) {
+    switch (event.type) {
+      case "message":
+        // ユーザーから送られたテキスト
+        var gotText = "";
+        // ユーザーに返すテキスト
+        var replyText = "";
+        var replyMessages = [];
+        if (event.message.type === "text") {
+        // テキストが送られた場合
+          gotText = event.message.text;
+          replyMessages = _makeSendMessages(gotText);
+          replyMessages.forEach(function(message) {
+            console.log("reply:" + message.text);
+          });
+        } else {
+        // スタンプ等が送られた場合
+          replyMessages = [
+            {
+              "type" : "text",
+              "text" : "ちょっと私には難しいなあ"
+            }
+          ];
+          replyMessages.forEach(function(message) {
+            console.log("reply:" + message.text);
+          });
+        }
+
+        // メッセージを返信
+        sender.send(event.replyToken, replyMessages);
+        break;
+      case "follow":
+        /* 友達追加・ブロック解除 */
+        break;
+      case "unfollow":
+        /* ブロック */
+        break;
+    }
+  });
+};
+
+function _makeSendMessages(gotText) {
+  const ADD = /登録|^と$/;
+  const RUN = /やる/;
+  const LIST = /みる|見る/;
+  var replyMessages = [];
+
+  // ルーチン登録
+  if (gotText.search(ADD) !== -1) {
+    replyMessages = [
+      {
+        "type" : "text",
+        "text" : "新しく登録するルーチン名を入力してね。"
+      },
+      {
+        "type" : "text",
+        "text" : "短い方が覚えやすいから嬉しいな"
+      }
+    ];
+    return replyMessages;
+  }
+
+  // ルーチン実行
+  var RUNindex = gotText.search(RUN);
+  if (RUNindex !== -1) {
+    replyMessages = [
+      {
+        "type" : "text",
+        "text" : gotText.substring(0, RUNindex) + "のルーチンを始めるんだね！"
+      },
+      {
+        "type" : "text",
+        "text" : "頑張ろう！"
+      }
+    ];
+    return replyMessages;
+  }
+
+  // ルーチン確認
+  var LISTindex = gotText.search(LIST);
+  if (LISTindex !== -1) {
+    replyMessages = [
+      {
+        "type" : "text",
+        "text" : gotText.substring(0, LISTindex) + "のルーチンに登録されているタスクは、こんな感じだよー"
+      },
+      {
+        "type" : "text",
+        "text" : "頑張ろう！"
+      }
+    ];
+    return replyMessages;
+  }
+  // default
+  replyMessages = [
+    {
+      "type" : "text",
+      "text" : "新しくルーチンを登録するには、「登録」とか「と」とか言ってね。"
+    }
+  ];
+  return replyMessages;
+};
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-const request = require('request');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const hkdnetHandler = require('./app/handlers/hkdnet.js');
+const tokimiHandler = require('./app/handlers/tokimi.js');
 
 app.use(morgan("dev", {immediate: true}));
 app.use(bodyParser.urlencoded({
@@ -13,95 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.post('/callback', function(req, res, next) {
-  // Web hookへのリクエストに200を返す
-  res.statusCode = 200;
-  res.end();
-
-  var events = req.body.events;
-  events.forEach(function(event) {
-    switch (event.type) {
-      case "message":
-      // ユーザーから送られたテキスト
-      var gotText = "";
-      // ユーザーに返すテキスト
-      var replyText = "";
-        if (event.message.type === "text") {
-        // テキストが送られた場合
-          gotText = event.message.text;
-          replyText = _getSendText(gotText);
-          console.log("replyText:" + replyText);
-        } else {
-        // スタンプ等が送られた場合
-          replyText = "ちょっと私には難しいなあ";
-          console.log("replyText:" + replyText);
-        }
-        gotText = event.message.text;
-        var postData = {
-          "replyToken" : event.replyToken,
-          "messages" : [
-            {
-              "type" : "text",
-              "text" : replyText
-            }
-          ]
-        };
-        var options = {
-          url: "https://api.line.me/v2/bot/message/reply",
-          headers: {
-            "Content-Type" : "application/json; charset=UTF-8",
-            "Authorization" : "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
-          },
-          json: true,
-          body: postData
-        };
-        request.post(options, function (err, res, body) {
-          if (!err && res.statusCode == 200) {
-            console.log(body);
-          } else {
-            console.log('error: ' + JSON.stringify(res));
-          }
-        });
-        break;
-      case "follow":
-        /* 友達追加・ブロック解除 */
-        break;
-      case "unfollow":
-        /* ブロック */
-        break;
-    }
-  });
-});
-
-_getSendText = function(gotText) {
-  const ADD = /登録|^と$/;
-  const RUN = /やる/;
-  const LIST = /みる|見る/;
-
-  var replyText = "ん？バグかな？";
-  
-  // ルーチン登録
-  if (gotText.search(ADD) !== -1) {
-    replyText = "新しく登録するルーチン名を入力してね。短い方が覚えやすいから嬉しいな";
-    return replyText;
-  }
-
-  // ルーチン実行
-  var RUNindex = gotText.search(RUN);
-  if (RUNindex !== -1) {
-    replyText = gotText.substring(0, RUNindex) + "のルーチンを始めるんだね！頑張ろう！";
-    return replyText;
-  }
-
-  // ルーチン確認
-  var LISTindex = gotText.search(LIST);
-  if (LISTindex !== -1) {
-    replyText = gotText.substring(0, LISTindex) + "のルーチンに登録されているタスクは、こんな感じだよー";
-    return replyText;
-  }
-  replyText = "新しくルーチンを登録するには、「登録」とか「と」とか言ってね。";
-  return replyText;
-};
+app.post('/callback', tokimiHandler);
 
 app.get('/test', function(req, res) {
   res.send("TOKIMI is AWAKE");
