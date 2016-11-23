1248071c195f16afd5e638a09bce0048ab6c9cd1
hkdnet <hkdnet@users.noreply.github.com>
2016-11-23 16:54:31 +0900

    taskの定義を新しくした

---
+++ models/task.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Task = sequelize.define('Task', {
+    userId: DataTypes.STRING,
+    taskName: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Task;
+};
